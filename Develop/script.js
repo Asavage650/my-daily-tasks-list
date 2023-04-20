55da87df8ced9ef35ca8c56768f4b09bc981d82f
{alexius <{alexiussavage0@gmail.com}>
2023-04-20 12:56:54 -0700

    finished readme and added images

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,23 @@
-
 $(function () {
-  $('#currentDay').text(dayjs().format("ddd MMMM D"))
-  
-  for (i = 9; i < 18; i++){
-    if (dayjs().hour()>i){
-      $("#hour-"+i).addClass("past") 
-     }
-     else if (dayjs().hour()===i){
-       $("#hour-"+i).addClass("present")
-   
-     }
-     else {
-       $("#hour-"+i).addClass("future")
-   
-     } 
+  $("#currentDay").text(dayjs().format("ddd MMMM D"));
+
+  for (i = 9; i < 18; i++) {
+    if (dayjs().hour() > i) {
+      $("#hour-" + i).addClass("past");
+    } else if (dayjs().hour() === i) {
+      $("#hour-" + i).addClass("present");
+    } else {
+      $("#hour-" + i).addClass("future");
+    }
   }
- 
-  $(document).ready(function (){
-    $('.saveBtn').on('click', function (){
-      var description = $(this).siblings('.description').val();
-      var time = $(this).parent().attr('id');
-      console.log(description)
+
+  $(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+      var description = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+      console.log(description);
       console.log(time);
       localStorage.setItem(time, description);
-    })
-  })
-  
-  
-
+    });
+  });
 });
